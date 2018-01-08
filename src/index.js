d8b4fa104942b4cc5a34a4744ea2023acbfaa7c5
alexgriff <alexalexgriffith@gmail.com>
2018-01-08 11:27:12 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+  const form = document.getElementById('identicon-form');
+  form.addEventListener('submit', ev => {
+    ev.preventDefault();
+
+    document.querySelectorAll('.fill').forEach(span => (span.className = ''));
+
+    const input = ev.target.querySelector('input');
+    const value = input.value;
+    input.value = '';
+    const setColor = 'blue';
+    identicon(value);
+  });
+});
