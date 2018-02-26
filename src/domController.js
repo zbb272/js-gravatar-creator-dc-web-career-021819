30cc4754162b1dc1436516c580f81ec53c6e7dc3
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2018-02-26 11:04:51 -0500

    Revert "clarify instructions"

--- src/domController.js
+++ src/domController.js
@@ -1,3 +1,17 @@
-function updateDOM() {
+function paintDOMNode(id, color) {
+  document.getElementById(id).style.backgroundColor = color
+}
 
+function mapMatrixToDOM(matrix, squareColor, blankColor='eee') {
+  matrix.forEach((row, rowIdx) => {
+    row.forEach((col, colIdx) => {
+      const color = col ? squareColor : blankColor
+      paintDOMNode(`${rowIdx}-${colIdx}`, color)
+    })
+  })
+}
+
+function updateDOM(inputValue) {
+  const identicon = new Identicon(inputValue)
+  mapMatrixToDOM(identicon.matrix, identicon.color)
 }
